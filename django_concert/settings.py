87aa6478879cde5ad16428bd198b8f86ddcb6c3e
saturn-sam <zaman.iu21@gmail.com>
2024-04-17 10:08:00 +0600

    Update settings.py

--- django_concert/settings.py
+++ django_concert/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-__a-u30pii=rp1qde9t@zdmykl=my#x0679qk#k895i7f#=+c=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '*.cognitiveclass.ai']
+ALLOWED_HOSTS = ['*','localhost', '127.0.0.1', '0.0.0.0', '*.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = ['https://*.cognitiveclass.ai']
 
 # Application definition
